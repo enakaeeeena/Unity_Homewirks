d2bc4f31f9b08734286f19d77e916770106ba383
enakaeeeena <166594070+enakaeeeena@users.noreply.github.com>
2024-10-31 14:02:18 +0300

    Добавила Задание 1 из кр

---
+++ Кр_1/Кр_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите действительное число: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        double rez = x - Math.Pow(x, 3) / Factorial(3) + Math.Pow(x, 5) / Factorial(5) - Math.Pow(x, 7) / Factorial(7) + Math.Pow(x, 9) / Factorial(9) - Math.Pow(x, 11) / Factorial(11) + Math.Pow(x, 13) / Factorial(13);
+
+        Console.WriteLine(rez);
+    }
+
+    static double Factorial(int n)
+    {
+        double result = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}
