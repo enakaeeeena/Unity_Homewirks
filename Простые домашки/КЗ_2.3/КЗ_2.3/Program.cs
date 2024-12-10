7eb4754a334ea4b3c65328ef8cbc5b68ed845a6f
enakaeeeena <166594070+enakaeeeena@users.noreply.github.com>
2024-12-10 14:37:26 +0300

    Добавила сложные домашки
    и кзд2.3 кз7.1

---
+++ Простые домашки/КЗ_2.3/КЗ_2.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число n:");
+        int n = int.Parse(Console.ReadLine());
+
+        int steps = CountSteps(n);
+        Console.WriteLine($"Количество замен, необходимых для достижения 1: {steps}");
+    }
+
+    static int CountSteps(int n)
+    {
+        int count = 0;
+
+        while (n != 1)
+        {
+            if (n % 2 == 0) 
+            {
+                n /= 2;
+            }
+            else 
+            {
+                n = 3 * n + 1;
+            }
+            count++;
+        }
+
+        return count;
+    }
+}
+
---
+++ Простые домашки/КЗ_7.1/КЗ_7.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace NumberValidator
+{
+    public class NumberException : Exception
+    {
+        public NumberException(string message) : base(message) { }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число в диапазоне от -2,147,483,648 до 2,147,483,647:");
+
+            try
+            {
+                int number = GetValidatedNumber(Console.ReadLine());
+                Console.WriteLine($"Вы ввели корректное число");
+            }
+            catch (NumberException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        public static int GetValidatedNumber(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new NumberException("Введено не число.");
+            }
+
+            if (!int.TryParse(input, out int number))
+            {
+                throw new NumberException("Введено не число.");
+            }
+
+            if (number < -2147483648)
+            {
+                throw new NumberException("Введено слишком маленькое число.");
+            }
+
+            if (number > 2147483647)
+            {
+                throw new NumberException("Введено слишком большое число.");
+            }
+
+            return number;
+        }
+    }
+}
+
